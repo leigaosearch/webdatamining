8f2339a954a7bfbce3224690b7c03c6416b52e35
gaolei <gaoleilucky@gmail.com>
2012-11-17 15:54:59 +0800

    new file:   filteringdata.cpp 	new file:   recommender.h

---
+++ recommender.h
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+typedef map<string,float> Ratings;
+typedef map<string,Ratings> Users;
+class recommender { 
+    public:
+	//load data from the given ratigns.
+	void loaddata(ifstream); 
+
+	//GetRatings
+	void GetRatings();
+
+	//compute the resulst find the kth near neighbours
+
+	//Pearson
+
+	//recommend	
+
+
+	//
+
+
+
+
+
+
+
+};
